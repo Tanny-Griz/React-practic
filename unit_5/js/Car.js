2bd92b1620ae8365826ea51d06f36c0dcdbc46a0
Tanya Kovalenko <tanya.griz89@gmail.com>
2020-01-06 23:19:27 +0200

    add 1-3 unit

--- unit_5/js/Car.js
+++ unit_5/js/Car.js
@@ -1,4 +1,4 @@
-// 1
+// 1 Создайте объект car и добавьте в него свойства model, weight, color и метод go(), который в консоль выводит строку 'go и model'.
 let car = {
     model: 'model',
     weight: 'weight',
@@ -10,7 +10,8 @@ let car = {
     }
 }
   
-// 2
+// 2 Создайте объект ford, добавьте свойство year. Выведите объект в консоль.
+// 6 Создайте у ford метод go(), который выводит информацию о годе производтся и цвете в консоль.
 let ford = {
     year: 1990,
     go: function () {
@@ -19,24 +20,30 @@ let ford = {
 }
 console.log(ford);
 
-//  3
+//  3 Задайте в качестве прототипа объекта ford, объект car. Выведите объект ford в консоль.
 // ford.__proto__ = car;
 Object.setPrototypeOf(ford, car);
 console.log(ford);
 
-// 4
+// 4 Выведите свойства ford.model, ford.weight, ford.color. Запустите метод ford.go().
 console.log(ford.model);
 console.log(ford.weight);
 console.log(ford.color);
 ford.go(); // 1990 color
 
-// 5
+// 5 Добавьте в car новое свойство mileage. Проверьте, появилось ли подобное свойство у ford?
 console.log(ford.mileage);
 
-// 7
+// 7 Вызовите у ford метод go принадлежащий прототипу.
 ford.__proto__.go(); // go model
 
-//  8 - 9 - 10
+//  Task 8 Создайте объект chevrolet, укажите в качестве прототипа объект ford. Проверьте наличие у chevrolet свойств model, color, mileage и метода go().
+
+// Task 9
+// Задайте свой метод go для chevrolet.
+
+// Task 10
+// Добавьте для chevrolet два свойства body_style, drive_type.
 let chevrolet = {
     body_style: 'сидан',
     drive_type: 'бензин',
@@ -53,7 +60,7 @@ console.log(chevrolet.color);
 console.log(chevrolet.mileage);
 chevrolet.go();
 
-// 11
+// 11 Переберите и выведите с помощью цикла все свойства объекта chevrolet.
 for (let key in chevrolet) {
     console.log(key + ": " + chevrolet[key]);
 }
